e82c1c01587fbe258f9772584a09bb04f881dbd2
scutaruandrei <scutaru.andrei@gmail.com>
2017-04-06 18:48:42 +0300

    Add files via upload

---
+++ seven_sg.ino
@@ -0,0 +1,29 @@
+
+#include "SevSeg.h"
+
+SevSeg sevseg; //Instantiate a seven segment controller object
+int outputpin= 0;
+void setup() {
+  Serial.begin(9600);
+  byte numDigits = 3;   
+  byte digitPins[] = {2, 3, 4};
+  byte segmentPins[] = {5,6,7,8,9,10};
+
+  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
+  sevseg.setBrightness(50);
+}
+
+void loop() {
+  static unsigned long timer = millis();
+  int rawvoltage= analogRead(outputpin);
+  float millivolts= (rawvoltage/1024.0) * 5000;
+  float celsius= millivolts/10;
+  Serial.print(celsius);
+ // sevseg.refreshDisplay(); // Must run repeatedly
+ // sevseg.setNumber(celsius,3); 
+  delay(100);
+  }
+
+
+
+/// END ///
